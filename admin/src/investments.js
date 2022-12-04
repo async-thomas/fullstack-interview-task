b8c6c4a70932abb527a4551de3aea286fde284d4
mint-made <thomaskupai@gmail.com>
2022-12-04 21:50:27 +0000

    Add fn for processing an investment report

--- admin/src/investments.js
+++ admin/src/investments.js
@@ -1,5 +1,23 @@
-const processInvestmentReport = () => {
-  return []
+const R = require("ramda")
+
+const processInvestmentReport = (investments, companies) => {
+  const mappedData = []
+  investments.forEach((investment) => {
+    investment.holdings.forEach((holding) => {
+      const company = R.find(R.propEq("id", holding.id))(companies)
+
+      const csvDataItem = [
+        investment.userId,
+        investment.firstName,
+        investment.lastName,
+        investment.date,
+        company.name,
+        holding.investmentPercentage * investment.investmentTotal,
+      ]
+      mappedData.push(csvDataItem)
+    })
+  })
+  return mappedData
 }
 
 module.exports.processInvestmentReport = processInvestmentReport
