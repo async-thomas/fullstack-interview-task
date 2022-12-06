f6e04f0f2d8e0e1f7b410933f449aa559f4e9817
mint-made <thomaskupai@gmail.com>
2022-12-06 08:38:58 +0000

    Refactor convDataArrToCsv fn

--- admin/src/util/utility.js
+++ admin/src/util/utility.js
@@ -1,14 +1,13 @@
 const R = require("ramda")
 
-const convDataArrToCsv = (dataArr) =>
-  R.pipe(
-    R.map((row) =>
-      R.pipe(
-        R.map((item) => JSON.stringify(item)),
-        R.join(","),
-      )(row),
-    ),
-    R.join("\n"),
-  )(dataArr)
+const convDataArrToCsv = R.pipe(
+  R.map((row) =>
+    R.pipe(
+      R.map((item) => JSON.stringify(item)),
+      R.join(","),
+    )(row),
+  ),
+  R.join("\n"),
+)
 
 module.exports.convDataArrToCsv = convDataArrToCsv
